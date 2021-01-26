ce23739fe26dcd9d2f45ef7406ea71da842c4d46
Massimiliano <m.croci.ly@gmail.com>
2021-01-26 15:05:10 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Ricreate lo slider visto in jQuery con vue. Personalizzatelo come meglio credete.
+
+var app = new Vue({
+  el : '#app',
+  data : {
+    images : [
+      'img/img-1.jpg',
+      'img/img-2.jpg',
+      'img/img-3.jpg',
+      'img/img-4.jpg'
+    ]
+  }
+});
