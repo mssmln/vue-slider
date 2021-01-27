603861677afbdcbe455f32b70a467f796b7909fd
Massimiliano <m.croci.ly@gmail.com>
2021-01-27 14:39:30 +0100

    finish off

--- js/script.js
+++ js/script.js
@@ -9,8 +9,20 @@ var app = new Vue({
       'img/img-2.jpg',
       'img/img-3.jpg',
       'img/img-4.jpg'
-    ]
+    ],
+    interval : '' // settata a null per poterci poi salvare un metodo
   },
+  // metodo mounted che parte dopo che l'istanza Vue è montata
+  mounted(){ // in questo caso non vedo differenze con created
+    this.interval = setInterval(this.nextImg,2000);
+  },
+  // created e mounted sono metodi Vue che partono in automatico, non serve intervenire sul html ne inserirli in methods
+
+  // metodo created che parte non appena l'istanza Vue è creata
+  // created(){ // ogni sezione è isolata a se, ti serve il this se vuoi richiamare un componente esterno
+  //   this.interval = setInterval(this.nextImg,2000); // sovrascriviamo interval inserendoci un metodo
+  // },
+
   methods : {
     nextImg(){
       // var conteggio = this.counter++;
@@ -48,6 +60,13 @@ var app = new Vue({
       // (this.counter < 1) ? this.counter = this.images.length - 1 : this.counter--;
       // console.log(this.counter);
     },
-    changeImg(){}
+    changeImg(indice){
+      this.counter = indice; // al click sul circle il counter avrà il valore dell'index che abbiamo usato con il v-for, quindi la img corrisponderà all'indice di images. Quando clicco vado a prendere l'indice e il counter diventa uguale all'indice.
+
+      // indice = this.counter; // così non appena clicchi su un pallino, lo slider si blocca
+
+      // quando clicchiamo su uno dei circle stoppiamo il setInterval
+      clearInterval(this.interval);
+    }
   }
 });
